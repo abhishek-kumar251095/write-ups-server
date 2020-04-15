33d0b704ea2750fc048b07d8cd80bf3b6425d150
abhishek-kumar251095 <abhishek.kumar251095@gmail.com>
2020-04-15 20:18:24 +0530

    Support for date-wise timeline entries

--- Timeline/Model/timelineModel.js
+++ Timeline/Model/timelineModel.js
@@ -1,26 +1,18 @@
 const mongoose = require('mongoose'),
       schema = mongoose.Schema;
 
-const timelineSchema = new schema({
-    userId: {
-        type: String
+const timelineSchema = new schema(
+    {
+        userId: {
+            type: String
+        },
+        dateObj: {year: Number, month: Number, date: Number, day: Number},
+        body: [
+            {title: String, activity: String, type: String, entryId: String}
+        ]
     },
-    entryId:{
-        type: String
-    },
-    type: {
-        type: String
-    },
-    date: {
-        type: Date
-    },
-    title: {
-        type: String
-    },
-    activity: {
-        type: String
-    }
-});
+    {typeKey: '$type'}
+);
 
 const timelineModel = mongoose.model("timeline", timelineSchema);
 module.exports = timelineModel;
--- Timeline/timeline.js
+++ Timeline/timeline.js
@@ -21,13 +21,50 @@ exports.postTimelineData = function(req,res){
 
     const userId = mongoose.Types.ObjectId(req.payload._id);
     req.body.userId = userId
+
+    let modifiedReq = transformRequest(req.body);
+
     timelineModel
-        .create(req.body)
+        .findOneAndUpdate(
+            {
+                "userId": modifiedReq.userId, 
+                "dateObj": modifiedReq.dateObj
+            },
+            {
+                "$set": {
+                    "userId": modifiedReq.userId, 
+                    "dateObj": modifiedReq.dateObj
+                },
+                "$push": {"body": modifiedReq.body}
+            },
+            {
+                upsert: true
+            }
+        )
         .then(result => {
             res.json(result);
         })
         .catch(err => {
             res.json(err);
         })
+}
+
+function transformRequest(req) {
+
+    let currDate = new Date(req.date);
+    let dateObj = {
+        year: currDate.getFullYear(),
+        month: currDate.getMonth(),
+        date: currDate.getDate(),
+        day: currDate.getDay()
+    } 
+
+    let transformedObj = {
+        userId: req.userId,
+        dateObj: dateObj,
+        body: {title: req.title, activity: req.activity, type: req.type, entryId: req.entryId}
+    }
+   
+    return transformedObj;
 
 }
